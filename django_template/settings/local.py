45117e6c8af52d3f4ad482c0cec74b99db0739de
Ryuji TSUTSUI <ryu22e@gmail.com>
2014-09-15 14:17:50 +0900

    Add development database

--- django_template/settings/local.py
+++ django_template/settings/local.py
@@ -3,3 +3,13 @@ from .base import *
 DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
