01051dfe5b50c64fa143d108c1fe56259f860969
Ryuji TSUTSUI <ryu22e@gmail.com>
2014-09-15 14:11:23 +0900

    Add production database

--- django_template/settings/production.py
+++ django_template/settings/production.py
@@ -1,5 +1,10 @@
 from .base import *
+import dj_database_url
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
