ad58bd898242601ec592d2fd3fa137841f244148
Ryuji TSUTSUI <ryu22e@gmail.com>
2015-06-24 23:24:16 +0900

    Reformat code

--- project_name/urls.py
+++ project_name/urls.py
@@ -3,17 +3,18 @@ from django.contrib import admin
 from django.conf import settings
 from home.views import HomeView
 
-urlpatterns = patterns('',
-                       # Examples:
-                       url(r'^$', HomeView.as_view(), name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-
-                       url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns(
+    '',
+    # Examples:
+    url(r'^$', HomeView.as_view(), name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += patterns('',
-                            url(r'^__debug__/', include(debug_toolbar.urls)),
+    urlpatterns += patterns(
+        '',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
     )
