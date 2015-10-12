972187ac7c5be8ca7452c778d1f147b7080f3ea5
Ryuji TSUTSUI <ryu22e@gmail.com>
2015-10-12 15:44:47 +0900

    Use In-Memory database at running tests

--- project_name/settings/test.py
+++ project_name/settings/test.py
@@ -9,12 +9,15 @@ DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'default.db'),
-    }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
 }
 
 # celery
