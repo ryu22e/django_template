7ecc8d8127ef5032465c1586fd78b438b3a5c518
Ryuji TSUTSUI <ryu22e@gmail.com>
2015-10-12 15:50:03 +0900

    Use fastest hasher to speed up tests

--- project_name/settings/test.py
+++ project_name/settings/test.py
@@ -23,3 +23,10 @@ DATABASES = {
 # celery
 # http://docs.celeryproject.org/en/latest/configuration.html#celery-always-eager
 CELERY_ALWAYS_EAGER = True
+
+# Use fastest hasher to speed up tests.
+# SECURITY WARNING: don't use this hasher in production!
+# https://docs.djangoproject.com/en/1.8/ref/settings/#std:setting-PASSWORD_HASHERS
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
